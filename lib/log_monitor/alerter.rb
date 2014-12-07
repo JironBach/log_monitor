d62dd2db683b972c254d3f8a31f6ce3aada32c23
JironBach <junshimo2@gmail.com>
2014-12-08 00:19:31 +0900

    debug tlsmail's problem

--- lib/log_monitor/alerter.rb
+++ lib/log_monitor/alerter.rb
@@ -101,15 +101,6 @@ module LogMonitor
 
 
   require 'mail'
-
-  Net.instance_eval {remove_const :SMTPSession} if defined?(Net::SMTPSession)
-  Net::POP.instance_eval {remove_const :Revision} if defined?(Net::POP::Revision)
-  Net.instance_eval {remove_const :POP} if defined?(Net::POP)
-  Net.instance_eval {remove_const :POPSession} if defined?(Net::POPSession)
-  Net.instance_eval {remove_const :POP3Session} if defined?(Net::POP3Session)
-  Net.instance_eval {remove_const :APOPSession} if defined?(Net::APOPSession)
-  require 'tlsmail'
-
   class EmailAlerter < Alerter
     def set_email(config)
       @config = config
